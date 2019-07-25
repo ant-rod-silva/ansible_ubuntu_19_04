a29b8e67f548dd3e6656b6d32c8029febef3fe8e
Antonio Rodrigo dos Santos Silva <20875150+rodrigosantosbr@users.noreply.github.com>
2019-07-25 12:11:09 -0300

    Update csv_to_openldap_import.py

--- roles/openldap/files/csv_to_openldap_import.py
+++ roles/openldap/files/csv_to_openldap_import.py
@@ -5,7 +5,7 @@ import ldap.modlist as modlist
  
 try:
     conn = ldap.initialize('ldap://localhost:389')
-    conn.simple_bind_s('cn=admin,dc=waqar,dc=local', 'admin_password')
+    conn.simple_bind_s('cn=admin,dc=example,dc=com', 'abcd1234')
  
 except ldap.LDAPError, e:
     print e
@@ -14,30 +14,33 @@ with open('data_file.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     uid = 48059
     for row in readCSV:
-        print "Adding " + str( row[0] + " " + row[1] + " " + row[2] + " " + row[3]+ " " + row[4]+ " " + row[5]+ " " + row[6]+ " " + row[7]+ " " + row[8])
  
-        dn = "uid="+str(row[4]) + ",ou=" + str(row[1]) + ",ou=" + str(row[0]) + ",ou=students,dc=waqar,dc=local"
+        dn = "uid={},{}".format(str(row[3]), "ou=People,dc=example,dc=com")
+        print "Adding: {}".format(dn)
  
         attrs = {}
-        attrs['objectclass'] = ['top','posixAccount','inetOrgPerson']
-        attrs['sn'] = str(row[6])
-        attrs['displayName'] = str(row[6])
-        attrs['givenName'] = str(row[6])
-        attrs['uid'] = str(row[4])
-        attrs['uidNumber'] = str(uid)
-        attrs['gidNumber'] = '0'
-        attrs['homeDirectory'] = str(row[4])
-        attrs['mail'] = str(row[8])
-        attrs['cn'] = str(row[6])
-        attrs['userPassword'] = str(row[5])
-        attrs['description'] = str(row[6]) + " Son of " + str(row[7]) + " Student of " + str(row[1]) + " of " + str(row[2])
- 
+        #attrs['objectclass'] = ['top','posixAccount','inetOrgPerson']
+        attrs['objectclass'] = ['ServidorPublicoObjectClass']
+        attrs['dn'] = str(dn)
+        attrs['givenName'] = str(row[1])
+        attrs['sn'] = str(row[2])
+        attrs['cn'] = str(row[3])
+        attrs['displayName'] = str(row[4])
+        attrs['description'] = str(row[5])
+        attrs['mail'] = str(row[6])
+        attrs['userPassword'] = str(row[7])
+        
+        #attrs['uid'] = str(row[4])
+        #attrs['uidNumber'] = str(uid)
+        #attrs['gidNumber'] = '0'
+        #attrs['homeDirectory'] = str(row[4])
+        
         ldif = modlist.addModlist(attrs)
  
         # print "ldif" + str(ldif)
         conn.add_s(dn,ldif)
  
-        uid = uid + 1
+        #uid = uid + 1
  
 conn.unbind_s()
 print "All Done"
